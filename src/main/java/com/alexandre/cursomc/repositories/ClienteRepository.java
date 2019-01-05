7bcfa4498df27f1e2a5a28648db3ca61648242ee
Alexandre Telles <alexandre.ntr@gmail.com>
2019-01-05 18:11:52 -0200

    Validação customizada email não pode ser repetido na inserção de Cliente

--- src/main/java/com/alexandre/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/alexandre/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,18 @@ package com.alexandre.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alexandre.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	/**
+	 * Ao fazer um método nesse padrão findBy... e o nome do atributo o Spring Data vai implementar o método
+	 * @param email
+	 * @return
+	 */
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
