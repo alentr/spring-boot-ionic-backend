8e8760856a7411678c64edc0290aa83c0d5c4caa
Alexandre Telles <alexandre.ntr@gmail.com>
2019-01-05 13:31:01 -0200

    Validado Categoria com Bean Validation

--- src/main/java/com/alexandre/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandre/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alexandre.cursomc.domain.Categoria;
+import com.alexandre.cursomc.dto.CategoriaDTO;
 import com.alexandre.cursomc.repositories.CategoriaRepository;
 import com.alexandre.cursomc.services.exceptions.DataIntegrityException;
 import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
