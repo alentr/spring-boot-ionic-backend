663c1df9fb2cfae7125107ce0d7272b5a4f0b4f5
Alexandre Telles <alexandre.ntr@gmail.com>
2019-07-15 17:49:53 -0300

    Correção em comentários
    Correção em comentários

--- src/main/java/com/alexandre/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alexandre/cursomc/resources/CategoriaResource.java
@@ -28,7 +28,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping(value="/categorias")
 @Api(value="API REST categorias")
-//Informe que qualquer domínio pode acessar a API
+//Informa que qualquer domínio pode acessar a API
 @CrossOrigin(origins="*")
 public class CategoriaResource {
 
@@ -52,9 +52,9 @@ public class CategoriaResource {
 	@ApiOperation(value="Cadastra uma categoria")
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
 		Categoria obj = service.fromDTO(objDTO);
+		
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
 	}
