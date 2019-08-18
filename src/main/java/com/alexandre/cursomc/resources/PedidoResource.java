55769b19de91bacf8c07448f355bf2876a63773d
Alexandre Telles <alexandre.ntr@gmail.com>
2019-08-17 21:24:06 -0300

    cliente so recupera os proprios pedidos

--- src/main/java/com/alexandre/cursomc/resources/PedidoResource.java
+++ src/main/java/com/alexandre/cursomc/resources/PedidoResource.java
@@ -5,16 +5,20 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.alexandre.cursomc.domain.Categoria;
 import com.alexandre.cursomc.domain.Pedido;
+import com.alexandre.cursomc.dto.CategoriaDTO;
 import com.alexandre.cursomc.services.PedidoService;
 
 import io.swagger.annotations.Api;
@@ -28,12 +32,12 @@ import io.swagger.annotations.ApiOperation;
 public class PedidoResource {
 
 	@Autowired
-	private PedidoService service;
+	private PedidoService pedidoService;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	@ApiOperation(value="Retorna um pedido")
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
-		Pedido obj = service.find(id);
+		Pedido obj = pedidoService.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
@@ -41,10 +45,23 @@ public class PedidoResource {
 	@RequestMapping(method=RequestMethod.POST)
 	@ApiOperation(value="Cadastra um pedido")
 	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
-		obj = service.insert(obj);
+		obj = pedidoService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	@ApiOperation(value="Retorna todos os pedido paginados")
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="instante") String orderBy,
+			@RequestParam(value="direction", defaultValue="DESC") String direction) {
+		
+		Page<Pedido> pedidos = pedidoService.findPage(page, linesPerPage, orderBy, direction);
+		
+		return ResponseEntity.ok().body(pedidos);
 	}
 }
--- src/main/java/com/alexandre/cursomc/services/PedidoService.java
+++ src/main/java/com/alexandre/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alexandre.cursomc.domain.Cliente;
 import com.alexandre.cursomc.domain.ItemPedido;
 import com.alexandre.cursomc.domain.PagamentoComBoleto;
 import com.alexandre.cursomc.domain.Pedido;
@@ -14,13 +18,15 @@ import com.alexandre.cursomc.domain.enums.EstadoPagamento;
 import com.alexandre.cursomc.repositories.ItemPedidoRepository;
 import com.alexandre.cursomc.repositories.PagamentoRepository;
 import com.alexandre.cursomc.repositories.PedidoRepository;
+import com.alexandre.cursomc.security.UserSS;
+import com.alexandre.cursomc.services.exceptions.AuthorizationException;
 import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
 
 	@Autowired
-	private PedidoRepository repo;
+	private PedidoRepository pedidoRepository;
 	
 	@Autowired
 	private BoletoService boletoService;
@@ -41,7 +47,7 @@ public class PedidoService {
 	private EmailService emailService;
 	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = repo.findById(id);
+		Optional<Pedido> obj = pedidoRepository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
@@ -58,7 +64,7 @@ public class PedidoService {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-		obj = repo.save(obj);
+		obj = pedidoRepository.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
 		
 		for (ItemPedido ip : obj.getItens()) {
@@ -73,5 +79,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado.");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
